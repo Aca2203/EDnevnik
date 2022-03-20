30b30436902593fd425fde5ae87d0071f0915ada
Aca2203 <92998645+Aca2203@users.noreply.github.com>
2022-03-20 13:39:23 +0100

    Update Program.cs

--- Elektronski dnevnik/EDnevnik/Program.cs
+++ Elektronski dnevnik/EDnevnik/Program.cs
@@ -19,7 +19,7 @@ namespace EDnevnik
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Upisnica());
+            Application.Run(new Login());
         }
     }
 }
